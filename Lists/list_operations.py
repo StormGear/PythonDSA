b5f5b29270fc20fb27ec5d10fb06806abe0a6055
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-22 15:05:29 +0000

    append list method

--- Lists/list_operations.py
+++ Lists/list_operations.py
@@ -1,23 +1,29 @@
-list1 = [10, 20, 40, 5.7]
+list1: list[int] = [10, 20, 40, 5.7,]
 
 # replacing an item
 list1[2] = 3.7
 
+# print(list1)
+
 # insert a new element
 list1.insert(2, "hello") 
 
-animals = ["monkey", "dog", "lion", "cat"]
+# print(list1)
+
+animals: list[str] = ["monkey", "dog", "lion", "cat"]
 
 # sort a list in ascending alphabetical order
-animals.sort()
+# animals.sort()
 
 # delete a particular item
-del animals[2]
+# del animals[2]
 
 # add item to the end of a list
 animals.append(1)
 
 print(animals)
+
+# print(animals) 
 
 # reverse the list
 animals.reverse()
