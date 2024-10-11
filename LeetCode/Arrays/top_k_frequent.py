9fdef54af3e471233537195f86f8fa11b59cdc87
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-10-11 10:09:17 +0000

    Top k frequent occurring element

---
+++ LeetCode/Arrays/top_k_frequent.py
@@ -0,0 +1,20 @@
+
+# time: O(n) space: O(n)
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        for num, count in count.items():
+            freq[count].append(num)
+
+        result = []
+        
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
