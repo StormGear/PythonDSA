3e15b4e76c8182a2ec647904e3efe3d0b7d08fd2
Akutteh <akuttehkelvin@gmail.com>
2024-12-28 13:58:45 +0000

    minimum rotated binary search

---
+++ BinarySearch/koko_eating_bananas.py
@@ -0,0 +1,70 @@
+from math import ceil
+
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        high = max(piles)
+        res = 0
+
+        for k in range(1, high+1):
+            time = 0
+            for i in piles:
+                if time > h:
+                    break
+                time_taken = ceil(i/k)
+                time += time_taken
+            if time <= h:
+                res = k
+                break
+
+        return res
+    
+
+    def minEatingSpeed2(self, piles: list[int], h: int) -> int:
+        high = max(piles)
+        res = high
+
+        k = list(range(1,high+1))
+
+        l, r = 0, len(k)-1
+        while l <= r:
+            m = l + ((r-l)//2)
+            # calculate the total time to eat all piles with rate k[m]
+            time = 0
+            for i in piles:
+                if time > h:
+                    break
+                time_taken = ceil(i/k[m])
+                time += time_taken
+
+            # move to the right or left of m depending on the total time 
+            if time > h:
+                l = m + 1
+            else:
+                res = min(res, k[m])
+                r = m - 1
+            
+        return res
+    
+    def minEatingSpeed3(self, piles: list[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = l + ((r-l)//2)
+            hours = 0
+            for p in piles:
+                hours += ceil(p/k)
+            
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+
+        return res
+    
+    
+
+if __name__ == '__main__':
+    soln = Solution()
+    print(soln.minEatingSpeed3([3,6,7,11], 8))  # 4
---
+++ BinarySearch/min_rotated.py
@@ -0,0 +1,48 @@
+# Problem minimum rotated sorted array
+class Solution:
+    def min_rotated_brute(self, nums: list[int]) -> int:
+        res = nums[0]
+
+        for num in nums:
+            res = min(res, num)
+
+        return res
+    
+    def min_rotated_binary(self, nums: list[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            # there is no pivot/break, it's infact increasing
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+
+            m = l + ((r-l)//2)
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
+    
+    def min_rotated_binary2(self, nums: list[int]) -> int:
+        l, r = 0, len(nums)-1
+
+        while l < r:
+            # let's find the pivot
+            m = l + ((r-l)//2)
+            
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m 
+        print('pivot:', l, 'value:', nums[l], 'r:', r, 'value:', nums[r], 'm:', m)
+        return nums[l]
+if __name__ == '__main__':
+    soln = Solution()
+    # print(soln.min_rotated_brute([3,4,5,6,1,2]))
+    # print(soln.min_rotated_binary([3,4,5,6,1,2]))
+    print(soln.min_rotated_binary2([3,4,5,6,1,2]))
+
