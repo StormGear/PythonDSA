6510a94a205058d888b6f16c6b06167e74481eb4
StormGear <papakofiboahen@gmail.com>
2025-01-13 06:39:34 +0000

    invert a binary tree

---
+++ Trees/BinaryTree/invert_tree.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invert_tree(self, root):
+        if root is None:
+            return None
+        root.left = self.invert_tree(root.right)
+        root.right = self.invert_tree(root.left)
+        return root
+    
+    def print_tree(self, root):
+        if root is None:
+            return
+        self.print_tree(root.left)
+        print(root.value, end=' ')
+        self.print_tree(root.right)
+
+if __name__ == '__main__':
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(9)
+
+    s = Solution()
+    s.print_tree(root)
+    print()
+    s.print_tree(s.invert_tree(root))
