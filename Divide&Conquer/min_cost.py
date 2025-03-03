272ff690e758873de62f5c779eab26d3e1896a2c
StormGear <papakofiboahen@gmail.com>
2025-03-03 11:51:06 +0000

    minimum cost

--- Divide&Conquer/min_cost.py
+++ Divide&Conquer/min_cost.py
@@ -1,4 +1,16 @@
 
+
+# Find the minimum cost to reach from the top-left cell to the bottom-right cell of a matrix
+# The cost of a cell is the number written in that cell
+# You can only move right or down from a cell
+# You have to start from the top-left cell and reach the bottom-right cell
+# You have to find the path that has the minimum cost
+# The cost of a path is the sum of the costs of the cells in that path  
+
+# Start from the upper indexes and move towards the lower indexes
+# At each cell, you have two options: move left or move up
+# Find the minimum cost of the two options
+# Add the cost of the current cell to the minimum cost of the two options
 def find_mincost(cost, m, n):
     if m < 0 or n < 0:
         return float('inf')
@@ -7,8 +19,7 @@ def find_mincost(cost, m, n):
     else:
         option1 = find_mincost(cost, m-1, n)
         option2 = find_mincost(cost, m, n-1)
-        option3 = find_mincost(cost, m-1, n-1)
-        return cost[m][n] + min(option1, option2, option3)
+        return cost[m][n] + min(option1, option2)
     
 
 if __name__=='__main__':
