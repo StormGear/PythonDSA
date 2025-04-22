43848afaa6b6f3ca79d8c10cde5462a15d3343c6
StormGear <papakofiboahen@gmail.com>
2025-04-22 16:06:37 +0000

    unique paths dp

---
+++ Dynamic_Programming/unique_paths.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for _ in range(m - 1):
+            new_row = [1] * n
+            for j in range(n-2, -1, -1):
+                new_row[j] = new_row[j+1] + row[j]
+            row = new_row
+
+        return row[0]
+        
+
+if __name__ == "__main__":
+    m = 3
+    n = 7
+    solution = Solution()
+    print(solution.uniquePaths(m, n))  # Output: 28
