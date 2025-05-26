2adad9aec0b9277e614308e18b8e0e5fde199336
StormGear <papakofiboahen@gmail.com>
2025-05-26 11:16:08 +0000

    insert intervals

--- Intervals/insert_intervals.py
+++ Intervals/insert_intervals.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     # Time complexity: O(n)
     # Space complexity: O(n)
@@ -25,6 +28,24 @@ class Solution:
             i += 1
         
         return merged
+    
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+
+        res.append(newInterval)
+
+        return res
+        
     
 if __name__ == '__main__':
     soln = Solution()
